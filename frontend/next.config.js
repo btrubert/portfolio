54c9bb1793d65bce6cb795a9b8737550abcb7e6d
btrubert <benjamin.trubert@gmail.com>
2020-11-12 15:40:16 +0000

    frontend at www (connection to backend not working)

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    env: {
-        SYMFONY_URL: "http://127.0.0.1:8000",
-    },
+    //env: {
+    //    SYMFONY_URL: ${SYMFONY_URL},
+    //},
     images: {
         domains: ["127.0.0.1"],
       },
