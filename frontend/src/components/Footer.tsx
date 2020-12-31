350dd3c2fc9be15f6019319a63b2470c4dcb45cb
btrubert <benjamin.trubert@gmail.com>
2020-12-31 12:03:07 +0100

    filter blog posts by language

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,4 +1,5 @@
 import { useTranslation } from 'utils/TranslationContext'
+import { useSession } from 'utils/SessionContext'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import Icon from '@mdi/react'
@@ -6,34 +7,39 @@ import { mdiTwitter, mdiInstagram, mdiGithub } from '@mdi/js'
 
 function Footer () {
     const [trans, dispatch] = useTranslation()
+    const [session, dispatchS] = useSession()
 
-    return <Navbar bg="dark" variant="dark" expand="lg" className="footer">
-        <Nav className="mr-auto">
-        <Nav.Item>
-            <img src="/Signature.png" className="signature" />
-        </Nav.Item>
-        </Nav>
-        <Nav className="justify-content-end">
-            <Nav.Item className="mr-2">
-                {trans.common._social_media} : 
+    if (session.loading) {
+        return <></>
+    } else {
+        return <Navbar bg="dark" variant="dark" expand="lg" className="footer">
+            <Nav className="mr-auto">
+            <Nav.Item>
+                <img src="/Signature.png" className="signature" />
             </Nav.Item>
-            <Nav.Item className="mr-2">
-                <a href="https://twitter.com/benjamintrubert" target="_blank">
-                    <Icon path={mdiTwitter} size={1} color="white" />
-                </a>
-            </Nav.Item>
-            <Nav.Item className="mr-2">
-                <a href="https://www.instagram.com/benjamintrubert/" target="_blank">
-                    <Icon path={mdiInstagram} size={1} color="white" />
-                </a>
-            </Nav.Item>
-            <Nav.Item className="mr-2">
-                <a href="https://github.com/btrubert/portfolio/" target="_blank">
-                    <Icon path={mdiGithub} size={1} color="white" />
-                </a>
-            </Nav.Item>
-        </Nav>
-    </Navbar>
+            </Nav>
+            <Nav className="justify-content-end">
+                <Nav.Item className="mr-2">
+                    {trans.common._social_media} : 
+                </Nav.Item>
+                <Nav.Item className="mr-2">
+                    <a href="https://twitter.com/benjamintrubert" target="_blank">
+                        <Icon path={mdiTwitter} size={1} color="white" />
+                    </a>
+                </Nav.Item>
+                <Nav.Item className="mr-2">
+                    <a href="https://www.instagram.com/benjamintrubert/" target="_blank">
+                        <Icon path={mdiInstagram} size={1} color="white" />
+                    </a>
+                </Nav.Item>
+                <Nav.Item className="mr-2">
+                    <a href="https://github.com/btrubert/portfolio/" target="_blank">
+                        <Icon path={mdiGithub} size={1} color="white" />
+                    </a>
+                </Nav.Item>
+            </Nav>
+        </Navbar>
+    }
 }
 
 export default Footer
